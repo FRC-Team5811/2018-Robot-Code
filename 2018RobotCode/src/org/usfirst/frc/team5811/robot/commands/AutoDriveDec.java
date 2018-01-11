b0e304a8ac60b5eb021800fb31dc283c9c1c04e3
Sam M <munrosamuel@gmail.com>
2018-01-11 08:11:11 -0500

    Updated the class

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveDec.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveDec.java
@@ -1,5 +1,32 @@
 package org.usfirst.frc.team5811.robot.commands;
 
-public class AutoDriveDec {
+import edu.wpi.first.wpilibj.command.Command;
 
-}
+public class AutoDriveDec extends Command{
+	int count;
+	int duration;
+	
+	public AutoDriveAcc(int count, int duration) {
+		this.count = count;
+		this.duration = duration;
+		//input duration length here, not sure how to do it yet.
+		//Automatically assign values through group
+	}
+	
+	protected void intialize() {
+		count = 0;
+		duration = 25;
+	}
+	
+	protected void execute() {
+		DriveTrain.autoDriveDec(duration, count);
+		count ++;
+	}
+	
+	protected boolean isFinished() {
+		if (count > duration) {
+			return true;
+		} else {
+			return false;
+		}
+	}
