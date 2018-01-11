52a3cb70ec8db344fc9d123e9bb8caf58b9c85b6
Sam M <munrosamuel@gmail.com>
2018-01-11 08:13:03 -0500

    Completed flat

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveFlat.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveFlat.java
@@ -1,5 +1,32 @@
 package org.usfirst.frc.team5811.robot.commands;
 
-public class AutoDriveFlat {
+import edu.wpi.first.wpilibj.command.Command;
 
-}
+public class AutoDriveFlat extends Command{
+	int count;
+	int duration;
+	
+	public AutoDriveAcc(int count, int duration) {
+		this.count = count;
+		this.duration = duration;
+		//input duration length here, not sure how to do it yet.
+		//Automatically assign values through group
+	}
+	
+	protected void intialize() {
+		count = 0;
+		duration = 25;
+	}
+	
+	protected void execute() {
+		DriveTrain.autoDriveFlat(duration, count);
+		count ++;
+	}
+	
+	protected boolean isFinished() {
+		if (count > duration) {
+			return true;
+		} else {
+			return false;
+		}
+	}
