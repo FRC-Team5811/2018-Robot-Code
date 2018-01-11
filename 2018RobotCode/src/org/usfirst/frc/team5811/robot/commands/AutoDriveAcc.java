e42fa881f36b15482b4ec80261578e8abbadb2ce
Sam M <munrosamuel@gmail.com>
2018-01-11 08:10:13 -0500

    Added  constructor and fixed our earlier error

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveAcc.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveAcc.java
@@ -2,11 +2,15 @@ package org.usfirst.frc.team5811.robot.commands;
 
 import org.usfirst.frc.team5811.robot.subsystems.DriveTrain;
 
-public class AutoDriveAcc {
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoDriveAcc extends Command{
 	int count;
 	int duration;
 	
-	public AutoDriveAcc() {
+	public AutoDriveAcc(int count, int duration) {
+		this.count = count;
+		this.duration = duration;
 		//input duration length here, not sure how to do it yet.
 		//Automatically assign values through group
 	}
