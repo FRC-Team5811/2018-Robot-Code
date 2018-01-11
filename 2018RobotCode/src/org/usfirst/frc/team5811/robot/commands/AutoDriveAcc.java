1ce98d25351cf946c000c662da4871e5a447dfa5
Sam M <munrosamuel@gmail.com>
2018-01-11 08:23:21 -0500

    dfdfg

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveAcc.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/commands/AutoDriveAcc.java
@@ -8,25 +8,25 @@ public class AutoDriveAcc extends Command{
 	int count;
 	int duration;
 	
-	public AutoDriveAcc(int count, int duration) {
-		this.count = count;
+	public AutoDriveAcc( int duration) {
+		
 		this.duration = duration;
 		//input duration length here, not sure how to do it yet.
 		//Automatically assign values through group
 	}
 	
 	protected void intialize() {
-		count = 0;
 		duration = 25;
+		count = duration;		
 	}
 	
 	protected void execute() {
 		DriveTrain.autoDriveAcc(duration, count);
-		count ++;
+		count --;
 	}
 	
 	protected boolean isFinished() {
-		if (count > duration) {
+		if (count < duration) {
 			return true;
 		} else {
 			return false;
