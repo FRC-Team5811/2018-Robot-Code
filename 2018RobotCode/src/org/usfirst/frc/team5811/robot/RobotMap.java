220d48f16611779c67eba22469c8d84f84b768cb
CraftyARL <aaron.rlewis26@gmail.com>
2018-01-20 13:24:03 -0500

    lel

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/RobotMap.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/RobotMap.java
@@ -2,17 +2,27 @@ package org.usfirst.frc.team5811.robot;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 public class RobotMap {
 	public static Victor motor0 = new Victor(0);
 	public static Victor motor1 = new Victor(1);
 	public static Victor motor2 = new Victor(2);
 	public static Victor motor3 = new Victor(3);
+	public static Victor motor4 = new Victor(4);
+	public static Victor motor5 = new Victor(5);
+	public static Victor motor6 = new Victor(6);
+	
+	//public static Potentiometer pivot = new Potentiometer();
+	
+	public static DoubleSolenoid rampRelease = new DoubleSolenoid(0,1);
+	
 	public static I2C arduino = new I2C(I2C.Port.kMXP, 58);
 	public static AHRS navx;
 
---
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/subsystems/Pivot.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team5811.robot.subsystems;
+
+public class Pivot {
+
+}
