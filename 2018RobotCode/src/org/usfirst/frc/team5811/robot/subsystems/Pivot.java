72c30f04c12ca372427f47fe4e6e08212f9a0a6e
Aeolus1364 <millerluke1364@gmail.com>
2018-01-20 14:10:07 -0500

    Update pivot subsystem

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/subsystems/Pivot.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/subsystems/Pivot.java
@@ -1,10 +1,37 @@
 package org.usfirst.frc.team5811.robot.subsystems;
 
+import org.usfirst.frc.team5811.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
 
 public class Pivot extends Subsystem {
 	
-	public static void changeAngle() {
+	static Potentiometer pivot = RobotMap.pivot;
+	static Victor motor6 = RobotMap.motor6;
+	static Victor motor7 = RobotMap.motor7;
+	static double tolerance = 1; //how close to target is acceptable, in degrees
+	static int state = 0; //position state of arm
+	static double difference; 
+	
+	public static double getAngle() {
+		return pivot.get();
+	}
+	
+	public static int getState() {
+		return state;
+	}
+	
+	public static void changeAngle(double angle, int stateIn) {
+		state = stateIn;
+		difference = angle - pivot.get();
+		
+		if (difference > 0) {
+			
+		}
+		
+		
 		
 	}
 	
