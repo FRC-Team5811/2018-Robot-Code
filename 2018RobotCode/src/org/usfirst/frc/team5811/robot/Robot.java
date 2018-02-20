43a7f328c7823d019a66eb17ab812a894522d8e4
haveANiceDay33 <munrosamuel@gmail.com>
2018-02-20 08:03:41 -0500

    Some cleanup

--- 2018RobotCode/src/org/usfirst/frc/team5811/robot/Robot.java
+++ 2018RobotCode/src/org/usfirst/frc/team5811/robot/Robot.java
@@ -112,7 +112,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Pivot current: ", driveSUB.monitorCurrent6());
 		SmartDashboard.putBoolean("Is the compressor on???: ", Robot.driveSUB.checkCP());
 		SmartDashboard.putNumber("Potentiometer Value: ", Robot.pivot.getAngle());
-		SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
+		//SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
 		SmartDashboard.putBoolean("Do we detect a cube?", Robot.driveSUB.detectsCube());
 		SmartDashboard.putNumber("Pivot motor speed: ", Robot.pivot.getMotor());
 		SmartDashboard.putNumber("Switch Goal - Current: ", Robot.pivot.differenceSwitchTrans());
@@ -147,7 +147,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void disabledPeriodic() {
 		gameData = DriverStation.getInstance().getGameSpecificMessage();
-		if(gameData == null) {
+		if(gameData == null || gameData == "") {
 			gameData = DriverStation.getInstance().getGameSpecificMessage();
 			//waiting for letter
 		}
@@ -245,7 +245,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Pivot current: ", driveSUB.monitorCurrent6());
 		SmartDashboard.putBoolean("Is the compressor on???: ", Robot.driveSUB.checkCP());
 		SmartDashboard.putNumber("Potentiometer Value: ", Robot.pivot.getAngle());
-		SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
+		//SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
 		SmartDashboard.putBoolean("Do we detect a cube?", Robot.driveSUB.detectsCube());
 		SmartDashboard.putNumber("Pivot motor speed: ", Robot.pivot.getMotor());
 		SmartDashboard.putNumber("Switch Goal - Current: ", Robot.pivot.differenceSwitchTrans());
@@ -278,7 +278,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("Pivot current: ", driveSUB.monitorCurrent6());
 		SmartDashboard.putBoolean("Is the compressor on???: ", Robot.driveSUB.checkCP());
 		SmartDashboard.putNumber("Potentiometer Value: ", -Robot.pivot.getAngle());
-		SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
+		//SmartDashboard.putNumber("Current CX value: ", Robot.driveSUB.returnCX());
 		SmartDashboard.putBoolean("Do we detect a cube?", Robot.driveSUB.detectsCube());
 		SmartDashboard.putNumber("Pivot motor speed: ", Robot.pivot.getMotor());
 		SmartDashboard.putNumber("Switch Goal - Current: ", Robot.pivot.differenceSwitchTrans());
